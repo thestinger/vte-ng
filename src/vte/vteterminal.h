3a8a6599aa1190f8caaf8f6ffa55c3d064c90f9e
Jelle van der Waa <jelle@vdwaa.nl>
2016-02-13 22:25:19 +0100

    add function for setting the text selections

--- src/vte/vteterminal.h
+++ src/vte/vteterminal.h
@@ -196,6 +196,10 @@ _VTE_PUBLIC
 void vte_terminal_select_all(VteTerminal *terminal) _VTE_GNUC_NONNULL(1);
 _VTE_PUBLIC
 void vte_terminal_unselect_all(VteTerminal *terminal) _VTE_GNUC_NONNULL(1);
+_VTE_PUBLIC
+void vte_terminal_select_text(VteTerminal *terminal, long start_col, long start_row,
+			      long end_col, long end_row) _VTE_GNUC_NONNULL(1);
+
 
 /* By-word selection */
 _VTE_PUBLIC
