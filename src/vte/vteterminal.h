dd74ae7c06e8888af2fc090ac6f8920a9d8227fb
Jelle van der Waa <jelle@vdwaa.nl>
2016-02-13 22:47:44 +0100

    expose function for getting the selected text

--- src/vte/vteterminal.h
+++ src/vte/vteterminal.h
@@ -204,7 +204,9 @@ void vte_terminal_set_selection_block_mode(VteTerminal *terminal,
 _VTE_PUBLIC
 void vte_terminal_select_text(VteTerminal *terminal, long start_col, long start_row,
 			      long end_col, long end_row) _VTE_GNUC_NONNULL(1);
-
+_VTE_PUBLIC
+char *
+vte_terminal_get_selection(VteTerminal *terminal) _VTE_GNUC_NONNULL(1);
 
 /* By-word selection */
 _VTE_PUBLIC
